98910bc4462933b2df528b1618a36499f03d7f59
Palam Enix <marco.palma@ucr.ac.cr>
2023-06-19 19:34:54 +0000

    creacion de tema custom

---
+++ project/web/themes/custom/propio/js/propio.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Propio behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.propio = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
