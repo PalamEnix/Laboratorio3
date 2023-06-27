b281d80f63e201b11d0479f557739f4bfd7028ca
Palam Enix <marco.palma@ucr.ac.cr>
2023-06-27 03:21:26 +0000

    content export

--- project/web/sites/default/settings.php
+++ project/web/sites/default/settings.php
@@ -778,3 +778,6 @@ if (file_exists(__DIR__ . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 
  if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
    include $app_root . '/' . $site_path . '/settings.local.php';
  }
+ $settings['config_sync_directory'] = '../config/sync';
+ $settings['default_content_deploy_content_directory'] = '../content';
+ $settings['default_content_deploy_content_directory'] = '/var/dcd/content';
